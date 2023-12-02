9a7c45341fc3ac39eab2140321c9ba08cfd37471
Ashley Rolleston <AshRolls@users.noreply.github.com>
2023-12-02 14:00:55 +0000

    solve part one (54601)

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -19,7 +19,46 @@ public class Day01 : BaseDay
 
     private void solve1()
     {
-        _partOne = "Not Solved";
+        var detect = new Dictionary<string, int>()
+        {
+            { "1", 1 },
+            { "2", 2 },
+            { "3", 3 },
+            { "4", 4 },
+            { "5", 5 },
+            { "6", 6 },
+            { "7", 7 },
+            { "8", 8 },
+            { "9", 9 },
+            { "0", 0 }
+        };
+
+        char digit1 = ' ';
+        char digit2 = ' ';
+        int total = 0;
+        foreach (string s in _input)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (detect.ContainsKey(s[i].ToString()))
+                {
+                    digit1 = s[i];
+                    break;
+                }
+            }
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (detect.ContainsKey(s[i].ToString()))
+                {
+                    digit2 = s[i];
+                    break;
+                }
+            }
+            
+            total += Int32.Parse(digit1.ToString()) * 10; 
+            total += Int32.Parse(digit2.ToString());        
+        }
+        _partOne = total.ToString();
     }
 
     public override ValueTask<string> Solve_2()
@@ -32,4 +71,6 @@ public class Day01 : BaseDay
     {
         _partTwo = "Not Solved";
     }
+}
 
+    
