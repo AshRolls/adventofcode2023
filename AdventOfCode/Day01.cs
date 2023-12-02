ddc1eafc3c9c743b96a04fb83f25cfa5210405cf
Ashley Rolleston <AshRolls@users.noreply.github.com>
2023-12-02 19:05:16 +0000

    solve part two

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode;
+﻿using System.Linq;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
@@ -19,44 +21,12 @@ public class Day01 : BaseDay
 
     private void solve1()
     {
-        var detect = new Dictionary<string, int>()
-        {
-            { "1", 1 },
-            { "2", 2 },
-            { "3", 3 },
-            { "4", 4 },
-            { "5", 5 },
-            { "6", 6 },
-            { "7", 7 },
-            { "8", 8 },
-            { "9", 9 },
-            { "0", 0 }
-        };
-
-        char digit1 = ' ';
-        char digit2 = ' ';
         int total = 0;
         foreach (string s in _input)
         {
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (detect.ContainsKey(s[i].ToString()))
-                {
-                    digit1 = s[i];
-                    break;
-                }
-            }
-            for (int i = s.Length - 1; i >= 0; i--)
-            {
-                if (detect.ContainsKey(s[i].ToString()))
-                {
-                    digit2 = s[i];
-                    break;
-                }
-            }
-            
-            total += Int32.Parse(digit1.ToString()) * 10; 
-            total += Int32.Parse(digit2.ToString());        
+            var nums = AoCHelper.GetIndividualNumsFromStr(s);
+            total += nums.First() * 10;
+            total += nums.Last();
         }
         _partOne = total.ToString();
     }
@@ -69,7 +39,76 @@ public class Day01 : BaseDay
 
     private void solve2()
     {
-        _partTwo = "Not Solved";
+        var detect = new Dictionary<string, int>()
+        {
+            { "1", 1 },
+            { "2", 2 },
+            { "3", 3 },
+            { "4", 4 },
+            { "5", 5 },
+            { "6", 6 },
+            { "7", 7 },
+            { "8", 8 },
+            { "9", 9 },
+            { "0", 0 },
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 }, 
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 }                           
+        };
+
+        char digit1 = ' ';
+        char digit2 = ' ';
+        int total = 0;
+        foreach (string s in _input)
+        {
+            bool found = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (found) break;
+                
+                foreach (String key in detect.Keys)
+                {
+                    if (i + key.Length <= s.Length)
+                    {
+                        if (s.Substring(i, key.Length) == key)
+                        {
+                            digit1 = detect[key].ToString()[0];
+                            found = true;
+                            break;
+                        }
+                    }
+                }                                         
+            }
+            
+            found = false;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (found) break;
+
+                foreach (String key in detect.Keys)
+                {
+                    if (i + key.Length <= s.Length)
+                    {
+                        if (s.Substring(i, key.Length) == key)
+                        {
+                            digit2 = detect[key].ToString()[0];
+                            found = true;
+                            break;
+                        }
+                    }
+                }               
+            }
+
+            total += Int32.Parse(digit1.ToString()) * 10;
+            total += Int32.Parse(digit2.ToString());
+        }
+        _partTwo = total.ToString();
     }
 }
 
