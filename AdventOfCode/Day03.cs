9c55cb3666e661b32aab8545c16d8a73807b4c22
Ashley Rolleston <AshRolls@users.noreply.github.com>
2023-12-03 15:51:10 +0000

    solve day 3 part 2

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -1,5 +1,7 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Transactions;
 
 namespace AdventOfCode;
 
@@ -21,49 +23,37 @@ public class Day03 : BaseDay
     }
 
     private const int _size = 140;
-
+    private List<(int, int)> _checkPos = new List<(int, int)>()
+    {
+        (-1,-1),(0,-1),(1,-1),
+        (-1,0) ,       (1,0),
+        (-1,1) ,(0,1) ,(1,1)
+    };
     private void solve1()
     {
-        char[,] engine = new char[_size,_size];
-        for (int y = 0; y < _size; y++)            
-        {            
-            for (int x = 0; x < _size; x++)
-            {
-                engine[x, y] = _input[x][y];
-            }
-        }
-
-        List<(int, int)> checkPos = new List<(int, int)>();
-        checkPos.Add((-1, -1));
-        checkPos.Add((0, -1));
-        checkPos.Add((1, -1));
-        checkPos.Add((-1, 0));
-        checkPos.Add((1, 0));
-        checkPos.Add((-1, 1));
-        checkPos.Add((0, 1));
-        checkPos.Add((1, 1));
-
+        char[,] engine = parseFile();
         int total = 0;
-        for (int x = 0; x < _size; x++)            
+        
+        for (int y = 0; y < _size; y++)
         {
             bool num = false;
             bool adjacent = false;
-            StringBuilder sb = new StringBuilder ();
-            for (int y = 0; y < _size; y++)
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < _size; x++)
             {
                 if (Char.IsNumber(engine[x, y]))
                 {
                     num = true;
                     sb.Append(engine[x, y]);
-                    if (checkPositions(engine, checkPos, x, y)) adjacent = true;
+                    if (checkPositions(engine, x, y)) adjacent = true;
                 }
                 else if (num == true)
-                {                                       
+                {
                     if (adjacent)
                     {
                         Int32.TryParse(sb.ToString(), out int val);
                         total += val;
-                        adjacent = false;                        
+                        adjacent = false;
                     }
                     num = false;
                     sb.Clear();
@@ -81,16 +71,30 @@ public class Day03 : BaseDay
         }
 
         _partOne = total.ToString();
-    }   
+    }
 
-    private bool checkPositions(char[,] engine, List<(int,int)> checkPos, int x, int y)
+    private char[,] parseFile()
     {
-        foreach ((int,int) pos in checkPos)
+        char[,] engine = new char[_size, _size];
+        for (int y = 0; y < _size; y++)
+        {
+            for (int x = 0; x < _size; x++)
+            {
+                engine[x, y] = _input[y][x];
+            }
+        }
+
+        return engine;
+    }
+
+    private bool checkPositions(char[,] engine, int x, int y)
+    {
+        foreach ((int, int) pos in _checkPos)
         {
             if ((x + pos.Item1 > 0 && x + pos.Item1 < _size) &&
                 (y + pos.Item2 > 0 && y + pos.Item2 < _size))
             {
-                if (engine[x + pos.Item1,y + pos.Item2] != '.' && !Char.IsNumber(engine[x + pos.Item1, y + pos.Item2]))
+                if (engine[x + pos.Item1, y + pos.Item2] != '.' && !Char.IsNumber(engine[x + pos.Item1, y + pos.Item2]))
                 {
                     return true;
                 }
@@ -107,6 +111,70 @@ public class Day03 : BaseDay
 
     private void solve2()
     {
-        _partTwo = "Not Solved";
+        char[,] engine = parseFile();
+        int total = 0;        
+        for (int y = 0; y < _size; y++)
+        {
+            for (int x = 0; x < _size; x++)
+            {
+                if (engine[x, y] == '*')
+                {
+                    if (tryGetNumbersFromPos(x, y, engine, out (int, int) gears))
+                    {
+                        total += gears.Item1 * gears.Item2;
+                    }
+                }
+            }
+        }
+
+        _partTwo = total.ToString();
     }
+
+    private bool tryGetNumbersFromPos(int x, int y, char[,] engine, out (int, int) gears)
+    {
+        List<int> numbers = new List<int>();
+        foreach ((int, int) pos in _checkPos)
+        {
+            int checkPosX = x + pos.Item1;
+            int checkPosY = y + pos.Item2;
+
+            if ((checkPosX >= 0 && checkPosX < _size) &&
+                (checkPosY >= 0 && checkPosY < _size))
+            {
+                int xOffset = 0;
+                if (Char.IsNumber(engine[checkPosX, checkPosY]))
+                {
+                    // move left until start of number
+                    while (checkPosX + xOffset >= 0 && Char.IsNumber(engine[checkPosX + xOffset - 1, checkPosY]))
+                    {                        
+                        xOffset--;
+                        if (checkPosX + xOffset - 1 < 0) break;
+                    }
+                    StringBuilder sb = new StringBuilder();
+                    int xPos = 0;                                        
+                    while (Char.IsNumber(engine[checkPosX + xOffset + xPos, checkPosY]))
+                    {
+                        sb.Append(engine[checkPosX + xOffset + xPos, checkPosY]);
+                        xPos++;
+                        if (checkPosX + xOffset + xPos > _size - 1) break;
+                    }
+                    
+                    Int32.TryParse(sb.ToString(), out int num);
+                    sb.Clear();
+                    numbers.Add(num);
+                }
+            }
+        }
+        List<int> noDuplicates = numbers.Distinct().ToList();
+        if (noDuplicates.Count == 2)
+        {
+            gears = (noDuplicates[0], noDuplicates[1]);
+            return true;
+        }
+        else
+        {
+            gears = (0, 0);
+            return false;
+        }
+    } 
 }
