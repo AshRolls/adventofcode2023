5b94c877f94576ec985f39f56f0a3a84e006bc16
Ashley Rolleston <AshRolls@users.noreply.github.com>
2023-12-05 12:50:04 +0000

    solve day 5 part 2

--- AdventOfCode/Day05.cs
+++ AdventOfCode/Day05.cs
@@ -1,4 +1,9 @@
-﻿namespace AdventOfCode;
+﻿
+using System.Collections.Concurrent;
+using System.Drawing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AdventOfCode;
 
 public class Day05 : BaseDay
 {
@@ -19,16 +24,62 @@ public class Day05 : BaseDay
 
     private void solve1()
     {
-        var seeds = AoCHelper.GetLongNumsFromStr(_input[0]);
+        long[] seeds = AoCHelper.GetLongNumsFromStr(_input[0]);
+        Dictionary<int, List<Map>> listMaps;
+        parseMaps(out listMaps);
+
+        long lowestLoc = long.MaxValue;
+        foreach (long seed in seeds)
+        {
+            long curLoc = getLocations(seed, 1, listMaps);
+            if (curLoc < lowestLoc) lowestLoc = curLoc;
+        }
+        
+        _partOne = lowestLoc.ToString();
+    }
+
+    static readonly object lockObject = new object();
+    private static long getLocations(long seed, long range, Dictionary<int, List<Map>> listMaps)
+    {
+        Console.Out.WriteLine("Checking seed {0} : range {1}", seed, range);
+        long lowestLoc = long.MaxValue;       
+        // brute force it! ugly
+        Parallel.For(0, range, i =>             
+        {
+            long cur = seed + i;
+            foreach (List<Map> listMap in listMaps.Values)
+            {
+                foreach (Map m in listMap)
+                {
+                    if (cur >= m.sourceMin && cur < m.sourceMax)
+                    {
+                        cur = m.destMin + (cur - m.sourceMin);
+                        break;
+                    }
+                }                           
+            }
+            lock (lockObject)
+            {
+                if (cur < lowestLoc) lowestLoc = cur;
+            }
+        }
+        );
+
+        return lowestLoc;
+    }
+
+    private void parseMaps(out Dictionary<int, List<Map>> listMaps)
+    {       
         int i = 0;
-        Dictionary<int, List<Map>> listMaps = new Dictionary<int, List<Map>>
+        listMaps = new Dictionary<int, List<Map>>
         {
             { 0, new List<Map>() }
         };
+        
         foreach (string s in _input.Skip(3))
-        {
+        {            
             if (s.Contains(':'))
-            {
+            {                
                 i++;
                 listMaps.Add(i, new List<Map>());
                 continue;
@@ -39,31 +90,16 @@ public class Day05 : BaseDay
             {
                 Map m = new Map(cur[0], cur[1], cur[2]);
                 listMaps[i].Add(m);
-            }            
-        }
-
-        List<long> locs = new List<long>();
-        foreach (long seed in seeds)
-        {
-            long cur = seed;
-            foreach (List<Map> listMap in listMaps.Values)
-            {
-                foreach(Map m in listMap)
-                {
-                    if (cur >= m.sourceMin && cur <= m.sourceMax)                        
-                    {
-                        cur = m.destMin + (cur - m.sourceMin);
-                        break;
-                    }
-                }
             }
-            locs.Add(cur);
         }
 
-        _partOne = locs.Min().ToString();
+        foreach(int m in listMaps.Keys)
+        {
+            listMaps[m].Sort((x, y) => x.sourceMin.CompareTo(y.sourceMin));
+        }
     }
 
-    private class Map
+    private struct Map
     {
         public long destMin { get; set; }
         public long destMax { get; set; }
@@ -79,7 +115,6 @@ public class Day05 : BaseDay
             sourceMax = source + r;
             range = r;
         }
-
     }
 
     public override ValueTask<string> Solve_2()
@@ -90,6 +125,17 @@ public class Day05 : BaseDay
 
     private void solve2()
     {
-        _partTwo = "Not Solved";
+        long[] seeds = AoCHelper.GetLongNumsFromStr(_input[0]);
+        Dictionary<int, List<Map>> listMaps;
+        parseMaps(out listMaps);
+
+        long lowestLoc = long.MaxValue;
+        for (int i = 0; i < seeds.Length; i += 2)
+        {           
+            long curLoc = getLocations(seeds[i], seeds[i + 1], listMaps);
+            if (curLoc < lowestLoc) lowestLoc = curLoc;
+        }
+
+        _partTwo = lowestLoc.ToString();
     }
 }
