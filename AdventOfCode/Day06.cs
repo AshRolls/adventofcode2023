30fa05273e048e9e66dab9f4b853dd6434b417c4
Ashley Rolleston <AshRolls@users.noreply.github.com>
2023-12-06 09:26:15 +0000

    solve day 6 part 1 and 2

---
+++ AdventOfCode/Day06.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+public class Day06 : BaseDay
+{
+    private readonly string[] _input;
+    private string _partOne;
+    private string _partTwo;
+
+    public Day06()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        solve1();
+        return new(_partOne);
+    }
+
+    private void solve1()
+    {        
+        int[] time = AoCHelper.GetNumsFromStr(_input[0]);
+        int[] dist = AoCHelper.GetNumsFromStr(_input[1]);
+        int[] waysToBeat = new int[time.Length];
+
+        for (int i = 0; i < time.Length; i++)
+        {      
+            for (int t = 1; t <= time[i]; t++)
+            {
+                if (t * (time[i] - t) > dist[i]) waysToBeat[i]++;
+            }
+        }
+
+        int prod = 1;
+        foreach (int v in waysToBeat) 
+        {
+            prod *= v;
+        }
+
+        _partOne = prod.ToString();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        solve2();
+        return new(_partTwo);
+    }
+
+    private void solve2()
+    {
+        long time = AoCHelper.GetLongNumsFromStr(Regex.Replace(_input[0], @"\s+", ""))[0];
+        long dist = AoCHelper.GetLongNumsFromStr(Regex.Replace(_input[1], @"\s+", ""))[0];
+        long waysToBeat = 0;
+
+        for (long t = 1; t <= time; t++)
+        {
+            long cur = t * (time - t);
+            if (cur > dist) waysToBeat++;
+        }     
+
+        _partTwo = waysToBeat.ToString();
+    }
+}
