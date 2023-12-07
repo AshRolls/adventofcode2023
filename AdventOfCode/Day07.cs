6b29ddedc4ef92b921a3ff3902e933075104ab23
Ashley Rolleston <AshRolls@users.noreply.github.com>
2023-12-07 11:06:36 +0000

    solve day 7 part 2

--- AdventOfCode/Day07.cs
+++ AdventOfCode/Day07.cs
@@ -22,41 +22,32 @@ public class Day07 : BaseDay
     private void solve1()
     {
         List<Hand> hands = new List<Hand>();
-        foreach (string s in _input)
-        {
-            Hand h = new Hand();
-            for (int i = 0; i < 5; i++)
-            {
-                h.cards[i] = parseCard(s[i]);
-            }
-            h.bid = Int32.Parse(s[6..]);
-            hands.Add(h);
-        }
+        parseHands(hands, false);
 
-        Dictionary<int,List<Hand>> handTypes = new Dictionary<int, List<Hand>>();
-        for(int i = 0; i<7; i++)
+        Dictionary<int, List<Hand>> handTypes = new Dictionary<int, List<Hand>>();
+        for (int i = 0; i < 7; i++)
         {
             handTypes[i] = new List<Hand>();
         }
 
         foreach (Hand h in hands)
         {
-            int singles = countDistinct(h);
+            int singles = h.cards.Distinct().Count();
             if (singles == 5) // high card
             {
-                handTypes[6].Add(h);                
+                handTypes[6].Add(h);
             }
             else if (singles == 4) // one pair
             {
-                handTypes[5].Add(h);                
+                handTypes[5].Add(h);
             }
             else if (singles == 1) // five of a kind
             {
-                handTypes[0].Add(h);                
+                handTypes[0].Add(h);
             }
             else if (singles == 2)
             {
-                if(countMaxGroup(h) == 4) // four of a kind
+                if (getMaxGroupCount(h) == 4) // four of a kind
                 {
                     handTypes[1].Add(h);
                 }
@@ -67,7 +58,7 @@ public class Day07 : BaseDay
             }
             else if (singles == 3)
             {
-                if (countMaxGroup(h) == 3) // three of a kind
+                if (getMaxGroupCount(h) == 3) // three of a kind
                 {
                     handTypes[3].Add(h);
                 }
@@ -79,35 +70,49 @@ public class Day07 : BaseDay
             else
             {
                 throw new Exception();
-            }                      
+            }
         }
 
+        _partOne = calcWinnings(handTypes).ToString();
+    }
+
+    private static int calcWinnings(Dictionary<int, List<Hand>> handTypes)
+    {
         int rank = 1;
         int winnings = 0;
         for (int i = 6; i >= 0; i--)
         {
             handTypes[i].Sort();
-            foreach(Hand h in handTypes[i])
+            foreach (Hand h in handTypes[i])
             {
                 winnings += h.bid * rank;
                 rank++;
             }
         }
 
-        _partOne = winnings.ToString();
+        return winnings;
     }
 
-    private int countMaxGroup(Hand h)
+    private void parseHands(List<Hand> hands, bool joker)
+    {
+        foreach (string s in _input)
+        {
+            Hand h = new Hand();
+            for (int i = 0; i < 5; i++)
+            {
+                h.cards[i] = parseCard(s[i], joker);
+            }
+            h.bid = Int32.Parse(s[6..]);
+            hands.Add(h);
+        }
+    }
+
+    private int getMaxGroupCount(Hand h)
     {
         return h.cards.GroupBy(x => x).Select(x => x.Count()).Max();
     }
 
-    private int countDistinct(Hand h)
-    {
-        return h.cards.Distinct().Count();
-    }
-
-    private int parseCard(char v)
+    private int parseCard(char v, bool joker)
     {
         if (Char.IsDigit(v)) return int.Parse(v.ToString());
         else
@@ -117,6 +122,7 @@ public class Day07 : BaseDay
                 case 'T':
                     return 10;
                 case 'J':
+                    if (joker) return 1;
                     return 11;
                 case 'Q':
                     return 12;
@@ -161,6 +167,71 @@ public class Day07 : BaseDay
 
     private void solve2()
     {
-        _partTwo = "Not Solved";
+        List<Hand> hands = new List<Hand>();
+        parseHands(hands, true);
+
+        Dictionary<int, List<Hand>> handTypes = new Dictionary<int, List<Hand>>();
+        for (int i = 0; i < 7; i++)
+        {
+            handTypes[i] = new List<Hand>();
+        }
+
+        foreach (Hand h in hands)
+        {
+            int distinct = h.cards.Distinct().Count(); 
+            int jokers = h.cards.Where(x => x == 1).Count();
+            if (distinct == 5) // high card
+            {
+                if (jokers == 0) handTypes[6].Add(h);
+                else handTypes[5].Add(h);
+
+            }
+            else if (distinct == 4) // one pair
+            {
+                if (jokers == 0) handTypes[5].Add(h);
+                else if (jokers >= 1) handTypes[3].Add(h);                               
+            }
+            else if (distinct == 1) // five of a kind
+            {
+                handTypes[0].Add(h);
+            }
+            else if (distinct == 2)
+            {
+                if (jokers == 0)
+                {
+                    if (getMaxGroupCount(h) == 4) // four of a kind
+                    {
+                        handTypes[1].Add(h);                                         
+                    }
+                    else // full house
+                    {
+                        handTypes[2].Add(h);
+                    }
+                }
+                else handTypes[0].Add(h);
+            }
+            else if (distinct == 3)
+            {
+                if (getMaxGroupCount(h) == 3) // three of a kind
+                {
+                    if (jokers == 0) handTypes[3].Add(h);
+                    else if (jokers == 1) handTypes[1].Add(h);
+                    else if (jokers == 2) handTypes[0].Add(h);
+                    else if (jokers == 3) handTypes[1].Add(h);
+                }
+                else // two pair
+                {
+                    if (jokers == 0) handTypes[4].Add(h);
+                    else if (jokers == 1) handTypes[2].Add(h);
+                    else if (jokers >= 2) handTypes[1].Add(h);
+                }
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        _partTwo = calcWinnings(handTypes).ToString();
     }
 }
